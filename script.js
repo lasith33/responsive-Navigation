ee638b555d0b9ccdb07f74229c2816b936794067
lasith chamuditha <125144995+lasith33@users.noreply.github.com>
2024-06-03 09:13:03 +0530

    add javascript fille

---
+++ script.js
@@ -0,0 +1,9 @@
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick =() =>{
+    searchForm.classList.toggle('active');
+}  
+
+let navbar=document.querySelector('.navbar ');
+document.querySelector('#mrnu-btn').onclick =() =>{
+    navbar.classList.toggle('active');
+}   
